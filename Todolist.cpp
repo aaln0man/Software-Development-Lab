eeeca3c6b3998966ec9655ff14c86bc33640b65e
Noman <aalncse@gmail.com>
2018-04-05 10:22:03 +0600

    main function

--- Todolist.cpp
+++ Todolist.cpp
@@ -8,3 +8,37 @@ struct todo
 {
 	char time[250], task[250];
 };
+
+int main()
+{
+	
+	
+	while(true)
+	{
+		cout << "a) Add Tasks\n";
+		cout << "b) Delete Tasks\n";
+		cout << "c) View Tasks\n";
+		cout << "d) Exit\n";
+
+		char option;
+		cin >> option;
+
+		switch(option)
+		{
+			case('d'):
+				exit(EXIT_SUCCESS);
+			case('c'):
+				viewTasks();
+				break;
+			case('b'):
+				deleteTask();
+				break;
+			case('a'):
+				addTask();
+				break;
+			default:
+				cout << "Invalid option!\n";
+		}
+	}
+	return 0;
+}

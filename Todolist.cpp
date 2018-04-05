dffc74f61d2bb64cbd68e17c4f8afa50889f3852
Noman <aalncse@gmail.com>
2018-04-05 10:27:00 +0600

    reduced the errors & added the functions to work with

--- Todolist.cpp
+++ Todolist.cpp
@@ -9,6 +9,48 @@ struct todo
 	char time[250], task[250];
 };
 
+
+
+void addTask()
+{
+	ofstream fileWrite("tasks.txt", ios::app | ios::binary);
+	struct todo myTask;
+	cout << "Insert task: ";
+	cin >> myTask.task;
+	cout << "Insert time: ";
+	cin >> myTask.time;
+
+	fileWrite.write((char *) &myTask, sizeof(myTask));
+	cout << "Added!\n";
+	fileWrite.close();
+}
+
+void viewTasks()
+{
+	ifstream fileRead("tasks.txt", ios::in | ios::binary);
+	if(!fileRead)
+	{
+		cout << "Cannot open file\n";
+		exit(EXIT_FAILURE);
+	}
+	struct todo myTask;
+	int i = 1;
+	while((fileRead.read((char *) &myTask, sizeof(struct todo))) != NULL)
+	{
+		cout << i++ << ": ";
+		cout << myTask.task << endl; 
+		cout << "Time: " << myTask.time << endl;
+		cout << endl;
+	}
+	cout << endl;
+	fileRead.close();
+}
+
+void deleteTask()
+{
+
+}
+
 int main()
 {
 	
